83ae0e35b47a0b97effe2a42a0429ca455cd0305
Dennis273 <dennisc695@gmail.com>
2018-06-21 01:09:37 +0800

    Add validator for username, password and email

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,43 @@
 import * as userController from './controller/user';
 import * as workController from './controller/work';
 import { Request, Response, NextFunction } from 'express';
+import { isAuthenticated } from './config/passport';
 import express from 'express';
+import { check, validationResult } from 'express-validator/check';
+import * as validator from './validator';
+
+let validate = (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            success: false,
+            errors: errors.array(),
+        });
+    } else next();
+};
+
 const router = express.Router();
 
-router.post('/login', userController.login);
-router.post('/register', userController.register);
+router.post('/login', [
+    validator.checkUsername,
+    validator.checkPassword,
+], validate ,userController.login);
+
+router.post('/register', [
+    validator.checkEmail,
+    validator.checkUsername,
+    validator.checkPassword,
+], validate, userController.register);
 router.all('/logout', userController.logout);
 
 router.get('/work/:id', workController.getWorkById);
 router.get('/work/all', workController.getAllWork);
 
 // require user login
-router.all('*', (req: Request, res: Response, next: NextFunction) => {
-    if (!req.user) res.status(403).end();
-    else next();
-});
+router.post('/work/new', isAuthenticated, workController.add);
+router.put('/work/update', isAuthenticated, workController.updateById);
+router.delete('/work/delete', isAuthenticated, workController.deleteById);
 
 
-router.post('/work/new', workController.add);
-router.put('/work/update', workController.updateById);
-router.delete('/work/delete', workController.deleteById);
 
 export default router;
